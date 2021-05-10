1ddf729b2576b6c55fec041494a93e8681230a0c
LewisCode-Art <dzackalewis@gmail.com>
2021-05-10 07:18:39 +0000

    new-branch

--- app.js
+++ app.js
@@ -1,43 +1,21 @@
-const http = require('http');
-const fs = require('fs');
+const path = require('path');
 
+const express = require('express');
+const bodyParser = require('body-parser');
 
+const app = express();
 
-const server = http.createServer((req, res) => {
-    const url = req.url;
-    const method = req.method
-    if (url === '/') {
-        res.write('<html>');
-        res.write('<head><title>Enter message</title></head>');
-        res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button>Send</button></form></body>');
-        res.write('</html>');
-        return res.end();
-    }
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-    if (url === '/message' && method === 'POST') {
-        const body = [];
-        req.on('data', (chunk) => {
-            console.log(chunk)
-            body.push(chunk);
-        });
-        return req.on('end', () => {
-            const parsedBody = Buffer.concat(body).toString();
-            console.log(parsedBody);
-            const message = parsedBody.split('=')[1];
-            fs.writeFile('message.txt', message, err => {
-                res.statusCode = 302;
-                res.setHeader('Location', '/');
-                return res.end();
-            });
-        });
-    };
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
 
-    res.setHeader('Content-Type', 'text/html');
-    res.write('<html>');
-    res.write('<head><title>My first Page</title></head>');
-    res.write('<body><h1>Hello from node js server! </h1></body>');
-    res.write('</html>');
-    res.end();
+app.use('/admin', adminData.routes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
-server.listen(3000);
+app.listen(3000);
